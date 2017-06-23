00e6b61d99df57144fc2484107cbdd001c1822f4
Fred <frederic.briolet@gmail.com>
2017-06-23 16:55:15 +0200

    Responsive and mini anim landing

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,18 +9,21 @@ function initVideosVolume () {
     video.addEventListener('mouseleave', function(){
       TweenMax.to(video, 1, {volume: 0});
     });
-
-    video.addEventListener('loadeddata', function() {
-      console.log(video.offsetWidth);
-      //video.style.height = video.offsetWidth + 'px';
-    });
-
-    // video.style.height = video.offsetWidth + 'px';
   });
 }
 
+
+function initLanding () {
+  TweenMax.to('.landing-logo', 0.8, {y:0, opacity:1});
+  TweenMax.staggerTo('.landing-text span:nth-of-type(1) i', 0.5, {x:0, y:0, opacity:1, rotation: 0, ease: Elastic.easeOut.config(1, 0.5), delay: 0.4}, 0.02);
+  TweenMax.staggerTo('.landing-text span:nth-of-type(2) i', 0.5, {x:0, y:0, opacity:1, rotation: 0, ease: Elastic.easeOut.config(1, 0.5), delay: 1.1}, 0.02);
+  TweenMax.staggerTo('.landing-text span:nth-of-type(3) i', 0.5, {x:0, y:0, opacity:1, rotation: 0, ease: Elastic.easeOut.config(1, 0.5), delay: 1.8}, 0.02);
+  TweenMax.staggerTo('.landing-text span:nth-of-type(4) i', 0.5, {x:0, y:0, opacity:1, rotation: 0, ease: Elastic.easeOut.config(1, 0.5), delay: 2.5}, 0.02);
+  TweenMax.to('.landing-btn', 0.8, {y:0, opacity:1, delay: 3.4});
+}
 function init() {
   initVideosVolume();
+  initLanding();
 }
 
 document.addEventListener('DOMContentLoaded', init);
